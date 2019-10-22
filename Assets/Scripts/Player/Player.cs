d7f7fdcb764200084fcae015c6c11298b27ea0c2
EgeDenizPekel <52773259+EgeDenizPekel@users.noreply.github.com>
2019-10-22 14:47:13 +0300

    Initial commit

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int health = 3;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void collidedWithEnemy(Enemy enemy) // Collided with the enemy
+    {
+        enemy.Attack(this);
+        if (health <= 0)
+        {
+            
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        Enemy enemy = col.collider.gameObject.GetComponent<Enemy>();
+        if (enemy)
+        {
+            collidedWithEnemy(enemy);
+        }
+    }
+}
